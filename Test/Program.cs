903c2128942b0676f6e7efbdc23c1960e63ad345
Vlarik <i>
2023-10-05 11:29:17 +0300

    Добавил измененный массив

--- Test/Program.cs
+++ Test/Program.cs
@@ -8,9 +8,30 @@
     }
 }
 
+string[] ModifiedArray(string[] array)
+{
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+            j++;
+    }
+    string[] modifiedArray = new string[j];
+    j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            modifiedArray[j] = array[i];
+            j++;
+        }
+    }
+    return modifiedArray;
+}
 
 Console.WriteLine("Введите кол-во элементов массива");
 int N = Convert.ToInt32(Console.ReadLine());
 string[] array = new string[N];
 FillArray(array);
 Console.WriteLine($"первоначальный массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"измененный массив: [{string.Join(", ", ModifiedArray(array))}]");
