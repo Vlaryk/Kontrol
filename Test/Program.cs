908015bb6ebd06067a855b78c5905b7a0210d202
Vlarik <i>
2023-10-05 11:18:21 +0300

    Задал массив

---
+++ Test/Program.cs
@@ -0,0 +1,16 @@
+﻿void FillArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Введите {i + 1}-й элемент массива");
+        string n = Console.ReadLine()!;
+        array[i] = n;
+    }
+}
+
+
+Console.WriteLine("Введите кол-во элементов массива");
+int N = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[N];
+FillArray(array);
+Console.WriteLine($"первоначальный массив: [{string.Join(", ", array)}]");
